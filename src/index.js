b7ee7425af0fabbb670a7a45e12fa047dcb389c3
Ivan Karpushkin <karpushkin_ioann@mail.ru>
2023-02-06 18:33:04 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+
+import Footer from './components/footer/footer';
+import TaskList from './components/task-list/task-list';
+import NewTaskForm from './components/new-task-form/new-task-form';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const todoData = [
+    {
+      description: 'Completed task',
+      created: 'created 17 seconds ago',
+      id: 'l1',
+      className: 'completed',
+    },
+    {
+      description: 'Editing task',
+      created: 'created 5 minutes ago',
+      id: 'l2',
+      className: 'editing',
+    },
+    {
+      description: 'Active task',
+      created: 'created 5 minutes ago',
+      id: 'l3',
+      className: 'completed',
+    },
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <NewTaskForm />
+      </header>
+      <section className="main">
+        <TaskList todos={todoData} />
+        <Footer />
+      </section>
+    </section>
+  );
+};
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
